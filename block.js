0e50989225bfa57faa94ab39e2cfb3e14fa50f7f
DiceMono <qrdfg@naver.com>
2019-06-12 16:51:09 +0900

    add keyboard event, requestAFrame, map

--- block.js
+++ block.js
@@ -1,114 +1,109 @@
 const canvas = document.getElementById('userCanvas');
 const ctx = canvas.getContext('2d');
+canvas.width = 200;
+canvas.height = canvas.width * 2;
+const blockSize = 10;
+const mapWidth = 10;
+const mapHeight = 20;
 
-const blockT1 = [[0, 0, 0],
-                [1, 1, 1],
-                [0, 1, 0]];
+const mapCtx = document.getElementById('mapCanvas').getContext('2d');
 
-const blockT2 = [[0, 1, 0],
-                [1, 1, 0],
-                [0, 1, 0]];
+let map = new Array();
+for (let i = 0; i < mapHeight; i++) {
+    map.push([0, i]);
+    map.push([mapWidth + 1, i]);
+} 
 
-const blockT3 = [[0, 1, 0],
-                [1, 1, 1],
-                [0, 0, 0]];
-
-const blockT4 = [[0, 1, 0],
-                [0, 1, 1],
-                [0, 1, 0]];
-
-const blockJ1 = [[0, 0, 0],
-                [1, 1, 1],  
-                [0, 0, 1]];
-
-const blockJ2 = [[0, 1, 0],
-                [0, 1, 0],
-                [1, 1, 0]];
-
-const blockJ3 = [[1, 0, 0],
-                [1, 1, 1],
-                [0, 0, 0]];
-
-const blockJ4 = [[0, 1, 1],
-                [0, 1, 0],
-                [0, 1, 0]];
-
-const blockZ1 = [[0, 0, 0],
-                [1, 1, 0],
-                [0, 1, 1]];
-
-const blockZ2 = [[0, 0, 1],
-                [0, 1, 1],
-                [0, 1, 0]];
-
-const blockO1 = [[1, 1, 0],
-                [1, 1, 0],
-                [0, 0, 0]];
-
-const blockS1 = [[0, 0, 0],
-                [0, 1, 1],
-                [1, 1, 0]];
-
-const blockS2 = [[0, 0, 1],
-                [0, 1, 1],
-                [0, 1, 0]];
-
-const blockL1 = [[0, 0, 0],
-                [1, 1, 1],
-                [1, 0, 0]];
-                
-const blockL2 = [[1, 1, 0],
-                [0, 1, 0],
-                [0, 1, 0]];
-
-const blockL3 = [[0, 0, 1],
-                [1, 1, 1],
-                [0, 0, 0]];
-                
-const blockL4 = [[0, 1, 0],
-                [0, 1, 0],
-                [0, 1, 1]];
-
-const blockI1 = [[0, 0, 0, 0],
-                [0, 0, 0, 0],
-                [1, 1, 1, 1],                
-                [0, 0, 0, 0]];
-                
-const blockI2 = [[0, 0, 1, 0],
-                [0, 0, 1, 0],
-                [0, 0, 1, 0],
-                [0, 0, 1, 0]];
-
-
-function Block(name, shapes, point, size) {
-    this.name = name;
-    this.shapes = shapes;
-    this.point = point || [1.1];
-    this.size = size || 3;
+for (let i = 0; i < mapWidth + 2; i++) {
+    map.push([i, mapHeight])
 }
 
-const blockT = new Block(name='T', shapes=[blockT1, blockT2, blockT3, blockT4]);
-const blockJ = new Block(name='J', shapes=[blockJ1, blockJ2, blockJ3, blockJ4]);
-const blockZ = new Block(name='Z', shapes=[blockZ1, blockZ2]);
-const blockO = new Block(name='O', shapes=[blockO1]);
-const blockS = new Block(name='S', shapes=[blockS1, blockS2]);
-const blockL = new Block(name='L', shapes=[blockL1, blockL2, blockL3, blockL4]);
-const blockI = new Block(name='I', shapes=[blockI1, blockI2], point=[2,2], size=4);
-console.log(blockT.point)
+for (let i = 0; i < map.length; i++) {
+    mapCtx.fillRect(map[i][0]* blockSize, map[i][1]* blockSize, blockSize-1, blockSize-1)
+}
 
-const currentBlock = {
-    block: blockT,
-    locationX: 3,
-    locationY: 0,
-    rotation: 5,
-    draw: function() {
-        ctx.fillStyle = 'rgb(0,150,0)'
-        for (let i = 0; i < this.block.size; i++) {
-            for (let j = 0; j < this.block.size; j++) {
-                if (this.block.shapes[this.rotation % this.block.shapes.length][j][i]) {
-                    ctx.fillRect((this.locationX+i) * 20, (this.locationY+j) * 20, 19, 19);
-                }
-            }
+
+const blockTShape = [[0,1], [1,1], [2,1], [1,2]];
+const blockJShape = [[0,1], [1,1], [2,1], [2,2]];
+const blockZShape = [[0,1], [1,1], [1,2], [2,2]];
+const blockOShape = [[0,1], [1,1], [0,2], [1,2]];
+const blockSSahpe = [[1,1], [2,1], [0,2], [1,2]];
+const blockLShape = [[0,1], [1,1], [2,1], [0,2]];
+const blockIShape = [[0,2], [1,2], [2,2], [3,2]];
+
+const current = {
+    shape: blockTShape,
+    midPoint: 2,
+    locationX: 4,
+    locationY: -1,
+    rotate: function(isClockwise) {
+        let sign = isClockwise ? 1 : -1;
+        let rotatedShape = new Array(4);
+        for (let i = 0; i < 4; i++) {
+            let x = -sign * (this.shape[i][1] - this.midPoint) + this.midPoint;
+            let y = sign * (this.shape[i][0] - this.midPoint) + this.midPoint;
+            rotatedShape[i] = new Array(x, y);
         }
-    }
+        this.shape = rotatedShape;
+    },
+    draw: function() {
+        ctx.fillStyle = 'rgb(0,150,0)';
+        for (let i = 0; i < 4; i++) {
+            ctx.fillRect((this.locationX + this.shape[i][0]) * blockSize, (this.locationY + this.shape[i][1]) * blockSize, blockSize - 1, blockSize -1);
+        }
+    },
+    clear: function() {
+        ctx.fillStyle = 'rgb(0,150,0)';
+        for (let i = 0; i < 4; i++) {
+            ctx.clearRect((this.locationX + this.shape[i][0]) * blockSize, (this.locationY + this.shape[i][1]) * blockSize, blockSize - 1, blockSize -1);
+        }
+    },
 }
+
+// keyboard event
+window.onkeydown = function(e) {
+    if (e.keyCode == 40) {
+        current.clear();
+        current.locationY++;
+        current.draw();
+    }
+    if (e.keyCode == 37) {
+        current.clear();
+        current.locationX--;
+        current.draw();
+    } 
+    if (e.keyCode == 39) {
+        current.clear();
+        current.locationX++;
+        current.draw();
+    } 
+    if (e.keyCode == 65) {
+        current.clear();
+        current.rotate(isClocwise=false);
+        current.draw();
+    }
+    if (e.keyCode == 83) {
+        current.clear();
+        current.rotate(isClockwise=true);
+        current.draw();
+    }  
+}
+
+
+let start = null;
+handler = function(timeStamp) {
+    if (!start) {
+        start = timeStamp;
+    }
+    current.draw();
+    if (timeStamp -  start> 1000) {
+        current.clear();
+        ++current.locationY;
+        start = timeStamp
+    }
+    requestAnimationFrame(handler);
+}
+
+requestAnimationFrame(handler);
+
+
