5481c73202ff375a37a70abf351cbf852572d9f1
DiceMono <qrdfg@naver.com>
2019-06-13 15:01:17 +0900

    modify gameBoard(Array Type -> Map Type)

--- block.js
+++ block.js
@@ -10,7 +10,7 @@ const mapCtx = document.getElementById('mapCanvas').getContext('2d');
 
 let map = new Array();
 for (let i = 0; i < mapHeight; i++) {
-    map.push([0, i]);
+    map.push([0,i]);
     map.push([mapWidth + 1, i]);
 } 
 
@@ -18,10 +18,43 @@ for (let i = 0; i < mapWidth + 2; i++) {
     map.push([i, mapHeight])
 }
 
-for (let i = 0; i < map.length; i++) {
-    mapCtx.fillRect(map[i][0]* blockSize, map[i][1]* blockSize, blockSize-1, blockSize-1)
-}
+// for (let i = 0; i < map.length; i++) {
+//     mapCtx.fillRect(map[i][0]* blockSize, map[i][1]* blockSize, blockSize-1, blockSize-1)
+// }
 
+let mb = new Array();
+for (let i = 0; i < mapHeight; i++) {
+    mb.push([i, [0, mapWidth + 1]]);
+} 
+mb.push([mapHeight, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]])
+const border = new Map(mb)
+console.log(mb)
+const gameBoard = {
+    border: border,
+    stack: new Map(),
+    getBoard: function() {
+        let board = new Map();
+        for (let i = 0; i < border.size; i++) {
+            let layer = this.border.get(i);
+            if (this.stack.has(i)) {
+                layer = layer.concat(this.stack.get(i));
+            }
+            board.set(i, layer);
+        }
+        return board;
+    },
+    draw: function() {
+        let board = this.getBoard();
+        for (let y = 0; y < board.size; y++) {
+            let layer = board.get(y);
+            for (let x of layer) {
+                mapCtx.fillRect(x * blockSize, y * blockSize, blockSize - 1, blockSize -1)
+            }
+        }
+    },
+} 
+gameBoard.draw()
+let a = gameBoard.getBoard();
 
 const blockTShape = [[0,1], [1,1], [2,1], [1,2]];
 const blockJShape = [[0,1], [1,1], [2,1], [2,2]];
@@ -60,18 +93,16 @@ const current = {
     },
     isConflict: function() {
         for (let i = 0; i < this.shape.length; i++) {
-            console.log(this.locationX + this.shape[i][0], this.locationY + this.shape[i][1])
             if (map.some(mapLocation => mapLocation[0] === this.locationX + this.shape[i][0] && mapLocation[1] === this.locationY + this.shape[i][1])) {
                 console.log('conflict!');
                 return true;
             }
         }  
-    }
-
+    },
 }
 
 // keyboard event
-window.onkeydown = function(e) {
+window.onkeyup= function(e) {
     if (e.keyCode == 40) {
         current.clear();
         current.locationY++;
@@ -124,7 +155,6 @@ window.onkeydown = function(e) {
     }  
 }
 
-
 const delay = 1000;
 let start = null;
 handler = function(timeStamp) {
@@ -136,14 +166,12 @@ handler = function(timeStamp) {
         ++current.locationY;
         if (current.isConflict()){
             --current.locationY;
-            current.draw();
+
             return;
         };
         current.draw();
         start = timeStamp
     }
-
-
     requestAnimationFrame(handler);
 }
 current.draw();
