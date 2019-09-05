ff92d9020e8e4aaad226eb4036b4617e2bdc311c
DiceMono <qrdfg@naver.com>
2019-09-05 22:33:30 +0900

    update tetris consumer

--- tetris/consumers.py
+++ tetris/consumers.py
@@ -1,29 +1,90 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
+from channels.exceptions import StopConsumer
+from channels.utils import await_many_dispatch
 from .models import Client
 from asyncio import sleep, wait, wait_for
 import json
+import functools
+from channels.layers import get_channel_layer
 
-ENEMY_NUMBER = 1
+CHANNEL_PREFIX = 'specific'
+
+
 class TetrisConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
+    async def __call__(self, receive, send):
+        """
+        Dispatches incoming messages to type-based handlers asynchronously.
+        """
+        # Initalize channel layer
+        self.channel_layer = get_channel_layer(self.channel_layer_alias)
+        if self.channel_layer is not None:
+            user = self.scope['user']
+            self.username = str(user)
+            if user.is_anonymous:
+                self.channel_name = await self.channel_layer.new_channel()
+            else:
+                self.channel_name = "%s.%s!%s" % (
+                    CHANNEL_PREFIX,
+                    self.channel_layer.client_prefix,
+                    self.username,
+                )
+            self.channel_receive = functools.partial(
+                self.channel_layer.receive, self.channel_name
+            )
+        # Store send function
+        if self._sync:
+            self.base_send = async_to_sync(send)
+        else:
+            self.base_send = send
+        # Pass messages in from channel layer or client to dispatch method
+        try:
+            if self.channel_layer is not None:
+                await await_many_dispatch(
+                    [receive, self.channel_receive], self.dispatch
+                )
+            else:
+                await await_many_dispatch([receive], self.dispatch)
+        except StopConsumer:
+            # Exit cleanly
+            pass
 
-        await self.accept()
-        await self.send(text_data=json.dumps({
-            'type': message
-        }))
-
-
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
+    async def join_room(self):
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'join_enemy',
+                'channel': self.channel_name.split('!')[1],
+                # TODO: handle username, channel name
+                'message': '%s entered room' % self.scope['user'],
+            }
+        )
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
+        group_members = await self.channel_layer.get_group_members((self.room_group_name))
+        print('member: ',group_members)
+        group_members.pop(self.channel_name)
+        enemies = [channel_name.split('!')[1] for channel_name in group_members.keys()]
 
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
+        await self.send(text_data=json.dumps({
+            'type': 'list_enemies',
+            'enemies': enemies
+        }))
 
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'tetris_%s' % self.room_name
+        print('hi', self.scope)
+
+        await self.accept()
+        await self.join_room()
+        await self.send(text_data=json.dumps({
+            'type': "get_local_token"
+        }))
+
+    async def leave_room(self):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
@@ -32,153 +93,74 @@ class TetrisConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'tetris_message',
-                'message': text_data_json
+                'type': 'leave_enemy',
+                'channel': self.channel_name.split('!')[1],
+                # TODO: handle username, channel name
+                'message': '%s leaved room' % self.scope['user'],
             }
         )
 
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+    async def disconnect(self, close_code):
+        await self.leave_room()
 
-    async def tetris_message(self, event):
+    async def is_enemy_exist(self):
+        member_count = await self.channel_layer.count_group_members(self.room_group_name)
+        if member_count > 1:
+            return True
+        return False
+
+    async def send_start_game(self):
+        if await self.is_enemy_exist():
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'start_game',
+                }
+            )
+        else:
+            await self.send(text_data=json.dumps({
+                'type': 'alert',
+                'message': 'can not start game'
+            }))
+
+    async def broadcast(self, text_data_json):
+        text_data_json['channel'] = self.channel_name.split('!')[1]
+        text_data_json['type'] = 'draw_enemy'
+        score = int(text_data_json['score'])
+        leaderboard = await self.channel_layer.update_score(self.room_group_name, self.channel_name, score)
+        text_data_json['leaderboard'] = leaderboard
+        await self.channel_layer.group_send_except_channel(self.room_group_name,
+                                                           self.channel_name,
+                                                           text_data_json)
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        type = text_data_json['type']
+        if type == 'send_start_game':
+            await self.send_start_game()
+        elif type == 'broadcast':
+            await self.broadcast(text_data_json)
+
+    async def _chat_event_handler(self, event):
         message = event['message']
 
-        # Send message to WebSocket
         await self.send(text_data=json.dumps({
+            'type': 'chat',
             'message': message
         }))
 
-# class TetrisConsumer(AsyncWebsocketConsumer):
-#     @database_sync_to_async
-#     def count_client(self):
-#         return Client.objects.filter(room_name=self.room_name).count()
-#
-#     @database_sync_to_async
-#     def create_client(self):
-#         return Client.objects.create(room_name=self.room_name, channel_name=self.channel_name)
-#
-#     @database_sync_to_async
-#     def delete_client(self):
-#         return Client.objects.get(channel_name=self.channel_name).delete()
-#
-#     @database_sync_to_async
-#     def get_enemy_channel_name(self):
-#         return Client.objects.filter(room_name=self.room_name).exclude(channel_name=self.channel_name)[0].channel_name
-#
-#     @database_sync_to_async
-#     def is_enemy_exist(self):
-#         return Client.objects.filter(room_name=self.room_name).exclude(channel_name=self.channel_name).exists()
-#
-#     async def connect(self):
-#         self.room_name = self.scope['url_route']['kwargs']['room_name']
-#         self.user = self.scope['user']
-#         print(self.user.id)
-#         a = 1
-#         a+=1
-#         print(a)
-#         count = await self.count_client()
-#         # if count > ENEMY_NUMBER:
-#         a+=1
-#         print(a)
-#         self.client = await self.create_client()
-#         await self.accept()
-#         a+=1
-#         print(a)
-#         await sleep(10)
-#
-#         is_exist = False
-#         while not is_exist:
-#             is_exist = await self.is_enemy_exist()
-#             print(is_exist)
-#             await sleep(10)
-#         a+=1
-#         print(a)
-#         self.enemy_channel = await self.get_enemy_channel_name()
-#
-#
-#
-#     async def disconnect(self, close_code):
-#         print('hi')
-#         await self.delete_client()
-#         # await self.channel_layer.group_discard(
-#         #     self.room_group_name,
-#         #     self.channel_name
-#         # )
-#
-#     async def receive(self, text_data):
-#         text_data_json = json.loads(text_data)
-#
-#         await self.channel_layer.group_discard(
-#             self.room_group_name,
-#             self.channel_name
-#         )
-#
-#         await self.channel_layer.group_send(
-#             self.room_group_name,
-#             {
-#                 'type': 'tetris_message',
-#                 'message': text_data_json
-#             }
-#         )
-#
-#         await self.channel_layer.group_add(
-#             self.room_group_name,
-#             self.channel_name
-#         )
-#
-#     async def tetris_message(self, event):
-#         message = event['message']
-#
-#         # Send message to WebSocket
-#         await self.send(text_data=json.dumps({
-#             'message': message
-#         }))
+    async def join_enemy(self, event):
+        event['type'] = 'join_enemy'
+        await self.send(text_data=json.dumps(event))
 
-# class TetrisConsumer(AsyncWebsocketConsumer):
-#     async def connect(self):
-#         self.room_name = self.scope['url_route']['kwargs']['room_name']
-#         self.room_group_name = 'tetris_%s' % self.room_name
-#
-#         await self.channel_layer.group_add(
-#             self.room_group_name,
-#             self.channel_name
-#         )
-#
-#         await self.accept()
-#
-#     async def disconnect(self, close_code):
-#         await self.channel_layer.group_discard(
-#             self.room_group_name,
-#             self.channel_name
-#         )
-#
-#     async def receive(self, text_data):
-#         text_data_json = json.loads(text_data)
-#
-#         await self.channel_layer.group_discard(
-#             self.room_group_name,
-#             self.channel_name
-#         )
-#
-#         await self.channel_layer.group_send(
-#             self.room_group_name,
-#             {
-#                 'type': 'tetris_message',
-#                 'message': text_data_json
-#             }
-#         )
-#
-#         await self.channel_layer.group_add(
-#             self.room_group_name,
-#             self.channel_name
-#         )
-#
-#     async def tetris_message(self, event):
-#         message = event['message']
-#
-#         # Send message to WebSocket
-#         await self.send(text_data=json.dumps({
-#             'message': message
-#         }))
+    async def leave_enemy(self, event):
+        event['type'] = 'leave_enemy'
+        await self.send(text_data=json.dumps(event))
+
+    async def start_game(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'start_game',
+        }))
+
+    async def draw_enemy(self, event):
+        await self.send(text_data=json.dumps(event))
